dd22801d96dde8b17bdc90194c03352abd22445f
ChurIQLab <churkin.vitaliy@mail.ru>
2026-08-11 23:38:30 +0300

    Add CIDR parsing to IPAddressValidator

--- IPCalculator/Sources/Extensions/Extension+UITextField.swift
+++ IPCalculator/Sources/Extensions/Extension+UITextField.swift
@@ -2,14 +2,14 @@ import UIKit
 
 extension UITextField {
     func applyIPFormatting(range: NSRange, replacementString string: String, validator: IPAddressValidatable) -> Bool {
-        let allowedCharacters = CharacterSet(charactersIn: "0123456789.")
+        let allowedCharacters = CharacterSet(charactersIn: "0123456789./")
         let characterSet = CharacterSet(charactersIn: string)
         if !allowedCharacters.isSuperset(of: characterSet), !string.isEmpty { return false }
 
         guard let currentText = self.text as NSString? else { return false }
         let newString = currentText.replacingCharacters(in: range, with: string)
 
-        if newString.count > 15 { return false }
+        if newString.count > 18 { return false }
 
         return validator.isValidIntermediateInput(newString)
     }
