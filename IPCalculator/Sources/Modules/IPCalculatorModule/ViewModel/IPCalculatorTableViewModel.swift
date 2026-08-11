648f9ec32ff052ece47d7f1fbefb899f90c222ed
ChurIQLab <churkin.vitaliy@mail.ru>
2026-08-11 20:34:05 +0300

    Show binary representation rows for address and netmask

--- IPCalculator/Sources/Modules/IPCalculatorModule/ViewModel/IPCalculatorTableViewModel.swift
+++ IPCalculator/Sources/Modules/IPCalculatorModule/ViewModel/IPCalculatorTableViewModel.swift
@@ -3,4 +3,11 @@ import Foundation
 struct IPCalculatorTableViewModel {
     let title: String
     let value: String
+    let isMonospaced: Bool
+
+    init(title: String, value: String, isMonospaced: Bool = false) {
+        self.title = title
+        self.value = value
+        self.isMonospaced = isMonospaced
+    }
 }
