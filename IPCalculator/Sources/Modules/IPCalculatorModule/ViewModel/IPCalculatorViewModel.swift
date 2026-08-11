f0ee4b865fc67b7086c017c07786bab53eb3c78c
ChurIQLab <churkin.vitaliy@mail.ru>
2026-08-11 20:34:05 +0300

    Show binary representation rows for address and netmask

--- IPCalculator/Sources/Modules/IPCalculatorModule/ViewModel/IPCalculatorViewModel.swift
+++ IPCalculator/Sources/Modules/IPCalculatorModule/ViewModel/IPCalculatorViewModel.swift
@@ -6,7 +6,9 @@ struct IPCalculatorViewModel {
     init(model: IPCalculationModel, formatter: IPAddressFormattable) {
         rows = [
             .init(title: "Address", value: formatter.string(from: model.ip)),
+            .init(title: "Address (bin)", value: formatter.binaryString(from: model.ip), isMonospaced: true),
             .init(title: "Netmask", value: formatter.string(from: model.netmask)),
+            .init(title: "Netmask (bin)", value: formatter.binaryString(from: model.netmask), isMonospaced: true),
             .init(title: "Wildcard", value: formatter.string(from: model.wildcard)),
             .init(title: "Network", value: formatter.string(from: model.network)),
             .init(title: "Broadcast", value: formatter.string(from: model.broadcast)),
